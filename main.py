8e420560d3844d45c00af14fa343747e10f2a287
Avb2 <114643524+Avb2@users.noreply.github.com>
2022-12-28 22:45:26 -0500

    Update main.py
    Added the ability to search for multiple stocks and target prices at once.

--- main.py
+++ main.py
@@ -3,100 +3,112 @@ import requests
 import sqlite3
 from datetime import datetime
 
+# URL
+url = 'https://www.google.com/finance?q='
+
 # Connect to the database and enable the cursor
 conn = sqlite3.connect('data-stocks.db')
 c = conn.cursor()
 
-# Take the stocks the user wants to search for and add them to the url
-users_stocks = [input('What stock are you looking for?')]
-url = 'https://www.google.com/finance?q='
+# user inputs stocks they want to search for
+users_stocks = input('What stock are you looking for?')
+stocks_split = users_stocks.split(',')
 
 # Target Price
+target_price = input('What is your target price?')
+target_price = target_price.split(',')
 
-target_price = float(input('What is your target price?'))
+# Count used to slice target price list
+count = 0
 
-# Loop begins, iterates through each stock and finds data off google finance. Then creates table with values for the stock information.
-for stock in users_stocks:
-    url += stock
-
-    # Requests to google finance
+# STOCK INFO
+for x in stocks_split:
+    url = f'{url}{x}'
     req = requests.get(url)
-
-    # Parse the URL with Beautiful Soup
     result = BeautifulSoup(req.text, 'html.parser')
 
     # Find the name of the stock. Use a try/ except statement to see if the stock is listed on google finance. If not, 'I couldnt find that will be printed to the user.'
     try:
         name = (result.find('div', {'class': 'zzDege'})).string
-        print(name)
         print(url)
 
-    except Exception:
-        print('I couldnt find that.')
+    except:
+        print('I couldnt find that!')
+        continue
 
-    # Find the Price of the stock.
+    # find the price of the stock and convert it into a float
     price = (result.find('div', {'class': 'YMlKec fxKbKc'})).string
     print(price)
-    pr = float(price[1:])
+    price_float = float(price[1:])
 
-    # Timestamp when the data is being logged.
+    # Timestamp when the data is being logged
     timestamp = str(datetime.now())[11:16]
 
-    # Date of when the data is being logged.
+    # Date of when the data is being logged
     date = str(datetime.now())[:11]
 
-    # Use a try/ except statement to test if a table with the same name already exists.If the table exists, the values will be added to the already existing table.
+
     try:
-        # Create table for the stock
-        c.execute(f'''CREATE TABLE {stock}
-        ("
-        Name,
-        Date,
-        Time,
-        Price
-        ")''')
+        # If the price of the stock is less than the specified target price, a watchlist table will be created in the database and values will be added
+        if price_float <= float(target_price[0 + count]):
+            # Creates a table called watchlist, if one isnt created already.
+            try:
+                c.execute('''CREATE TABLE Watchlist
+                    ("
+                    Name,
+                    Date,
+                    Time,
+                    Price
+                    ")''')
 
-        conn.commit()
 
-    # If the table already exists, values will be inserted into the table.
-    except Exception:
+            except:
+                pass
+
+            # Add values to the database
+            c.execute(f'''INSERT INTO Watchlist VALUES
+                ("
+                {name},
+                {date},                    
+                {timestamp},
+                ${price_float}
+                ")''')
+
+            conn.commit()
+
+    except:
         pass
 
-    # Add values to the database
-    c.execute(f'''INSERT INTO {stock} VALUES
-            ("
-            {name},
-            {date},
-            {timestamp},
-            ${pr}
-            ")''')
-
-    conn.commit()
-
-    # if the price is below the target price, a new table called WatchList will be created.
-    if pr <= target_price:
-        try:
-            # Create table for the stock
-            c.execute('''CREATE TABLE Watchlist
+        # Use a try/ except statement to test if a table with the same name already exists.If the table exists, the values will be added to the already existing table.
+    try:
+        # Create table for the stock
+        c.execute(f'''CREATE TABLE {x}
             ("
             Name,
-            Date,
+            Date,    
             Time,
             Price
             ")''')
 
+        conn.commit()
 
-        except:
+    except Exception:
             pass
 
-        # Add values to the database
-        c.execute(f'''INSERT INTO Watchlist VALUES
-            ("
-            {name},
-            {date},
-            {timestamp},
-            ${pr}
-            ")''')
+    # Add values to the database
+    c.execute(f'''INSERT INTO {x} VALUES
+        ("
+        {name},
+        {date},
+        {timestamp},
+        ${price_float}
+        ")''')
 
-        conn.commit()
-        
+    conn.commit()
+
+    # URL reset
+    url = 'https://www.google.com/finance?q='
+
+    # Count increases for the next loop
+    count += 1
+    print('finished')
