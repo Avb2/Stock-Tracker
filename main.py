3695155edcac9de0e5c54ef2cab70f9eb13db2fc
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-01-02 21:26:52 -0500

    Update main.py
    Added Tkinter GUI, as well as slight clean code fixes.

--- main.py
+++ main.py
@@ -3,108 +3,146 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3
 from datetime import datetime
+from tkinter import *
 
+# Initiates Tkinter
+root = Tk()
 
-# URL
+# Original URL
 url = 'https://www.google.com/finance?q='
 
 # Connect to the database and enable the cursor
 conn = sqlite3.connect('data-stocks.db')
 c = conn.cursor()
 
-# user inputs stocks they want to search for
-users_stocks = input('What stock are you looking for?')
-stocks_split = users_stocks.split(',')
-
-# Target Price
-target_price = input('What is your target price?')
-target_price = target_price.split(',')
-
-# STOCK INFO
-for count, x in enumerate(stocks_split):
-    url = f'{url}{x}'
-    req = requests.get(url)
-    result = BeautifulSoup(req.text, 'html.parser')
-
-    # Find the name of the stock. Use a try/ except statement to see if the stock is listed on google finance. If not, 'I couldnt find that will be printed to the user.'
-    try:
-        name = (result.find('div', {'class': 'zzDege'})).string
-        print(url)
-
-    except AttributeError:
-        print('I couldnt find that!')
-
-    # find the price of the stock and convert it into a float
-    price = (result.find('div', {'class': 'YMlKec fxKbKc'})).string
-    print(price)
-    price_float = float(price[1:])
-
-    # Timestamp when the data is being logged
-    timestamp = str(datetime.now())[11:16]
-
-    # Date of when the data is being logged
-    date = str(datetime.now())[:11]
 
 
-    try:
-        # If the price of the stock is less than the specified target price, a watchlist table will be created in the database and values will be added
-        if price_float <= float(target_price[count]):
-            # Creates a table called watchlist, if one isnt created already.
-            try:
-                c.execute('''CREATE TABLE Watchlist
+# 'Stock' Label for the entry widget
+stock_label = Label(root, text='Stocks ')
+stock_label.grid(row=0, column=0)
+
+# Input the stocks into the Entry widget labeled 'Stocks'
+users_stocks = Entry(root)
+users_stocks.grid(row=0, column=1)
+
+# 'Target price' Label for the entry widget
+target_price_label = Label(root, text='Target Price')
+target_price_label.grid(row=1, column=0)
+
+# Input for the price the user wants the stock to reach
+target_price = Entry(root)
+target_price.grid(row=1, column=1)
+
+# Enter Button, when clicked the inputs will be collected, and separated, then web scraped from 'Google.com/finance' using BeautifulSoup4. The values will then be added to the database.
+button_enter = Button(root, text='Enter', command=lambda: search_it(users_stocks, target_price))
+button_enter.grid(row=1, column=2)
+
+
+def search_it(STOCKS, TARGETPRICE):
+    # User inputs stocks they want to search for, the input is then split
+    users_stocks = str(STOCKS.get())
+    stocks_split = users_stocks.split(',')
+    print(stocks_split)
+
+    # User inputs the target price they hope the stock reaches, the input is then split
+    target_price = TARGETPRICE.get()
+    target_price = target_price.split(',')
+    print(target_price)
+
+    # STOCK INFO
+    for count, x in enumerate(stocks_split):
+        # New URL with the stock added to it
+        url = f'https://www.google.com/finance?q={x}'
+
+        # Requests and scrape the URL
+        req = requests.get(url)
+        result = BeautifulSoup(req.text, 'html.parser')
+
+        # Find the name of the stock. Use a try/ except statement to see if the stock is listed on Google finance. If not, 'I couldnt find that will be printed to the user' will be printed. If an Attribute Error occurs,'I couldnt find that!' will be printed.
+        try:
+            name = (result.find('div', {'class': 'zzDege'})).string
+            print(url)
+        except AttributeError:
+            print('I couldnt find that!')
+
+        # Finds the price of the stock and converts it into a float
+        price = (result.find('div', {'class': 'YMlKec fxKbKc'})).string
+        print(price)
+        price_float = float(price[1:])
+
+        # Timestamp when the data is being logged
+        timestamp = str(datetime.now())[11:16]
+
+        # Date of when the data is being logged
+        date = str(datetime.now())[:11]
+
+        # Use a try/ except statement to test if a table with the same name already exists.If the table exists, the values will be added to the already existing table.
+        try:
+            # Create table for the stock
+            c.execute(f'''CREATE TABLE {x}
                     (
                     Name,
-                    Date,
+                    Date,    
                     Time,
                     Price
                     )''')
 
+            conn.commit()
 
-            except OperationalError:
-                print('Operational Error: Could not create table.')
+        except OperationalError:
+            print('Operational Error: Could not create table.')
 
-            # Add values to the database
-            c.execute(f'''INSERT INTO Watchlist VALUES
+        # Adds the values to the database
+        c.execute(f'''INSERT INTO {x} VALUES
                 (
                 "{name}",
-                "{date}",                    
+                "{date}",
                 "{timestamp}",
                 "${price_float}"
                 )''')
-
-            conn.commit()
-
-    except OperationalError:
-        print('Operational Error: Could not insert values into the watchlist.')
-
-        # Use a try/ except statement to test if a table with the same name already exists.If the table exists, the values will be added to the already existing table.
-    try:
-        # Create table for the stock
-        c.execute(f'''CREATE TABLE {x}
-            (
-            Name,
-            Date,    
-            Time,
-            Price
-            )''')
-
         conn.commit()
 
-    except OperationalError:
-        print('Operational Error: Could not create table.')
+        # 'Finished' will be printed when the values have been added.
+        print('finished')
 
-    # Add values to the database
-    c.execute(f'''INSERT INTO {x} VALUES
-        (
-        "{name}",
-        "{date}",
-        "{timestamp}",
-        "${price_float}"
-        )''')
+        try:
+            # If the price of the stock is less than the specified target price, a watchlist table will be created in the database and values will be added
+            if price_float <= float(target_price[count]):
 
-    conn.commit()
+                # Creates a table called watchlist if one isnt created already. If an Operational Error occurs, 'Could not create table' will be displayed.
+                try:
+                    c.execute('''CREATE TABLE Watchlist
+                        (
+                        Name,
+                        Date,
+                        Time,
+                        Price
+                        )''')
 
-    # URL reset
-    url = 'https://www.google.com/finance?q='
 
-    print('finished')
+                except OperationalError:
+                    print('Operational Error: Could not create table.')
+
+                # Adds values to the Watchlist database if the target price is greater than the current price
+                c.execute(f'''INSERT INTO Watchlist VALUES
+                    (
+                    "{name}",
+                    "{date}",                    
+                    "{timestamp}",
+                    "${price_float}"
+                    )''')
+
+                conn.commit()
+
+                # Tells the user that the stock was added to the watchlist
+                print('Stock added to watchlist')
+
+        # IF an operational error occurs, 'Could not insert values into the watchlist' will be displayed
+        except OperationalError:
+            print('Operational Error: Could not insert values into the watchlist.')
+
+        # URL resets to the original url for the next loop
+        url = 'https://www.google.com/finance?q='
+
+
+root.mainloop()
