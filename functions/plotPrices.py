ea00553b31d39fb7c5237bdf6252d9334e508e21
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-20 22:30:34 -0400

    Update plotPrices.py
    - Price high color is now larger & plotted with magenta to stand out from other scatter plots

--- functions/plotPrices.py
+++ functions/plotPrices.py
@@ -1,45 +1,58 @@
 import matplotlib
 from matplotlib import pyplot
 import sqlite3
+from sqlite3 import OperationalError
 
 
 def showPlot(stock):
-    conn = sqlite3.connect('data-stocks.db')
+    conn = sqlite3.connect('/Users/ab/PycharmProjects/stock-tracker/data-stocks.db')
     c = conn.cursor()
 
-    c.execute(f'SELECT Price FROM {stock} WHERE Price>0')
-    AllPrices = c.fetchall()
+    try:
+        c.execute(f'SELECT Price FROM {stock} WHERE Price>0')
+        AllPrices = c.fetchall()
 
-    price = [float(prices[0].replace('$', '')) for prices in AllPrices]
-    print(price)
+        price = [float(prices[0].replace('$', '')) for prices in AllPrices]
+        print(price)
 
-    c.execute(f'SELECT Time FROM {stock}')
-    AllTimes = c.fetchall()
+        c.execute(f'SELECT Time FROM {stock}')
+        AllTimes = c.fetchall()
 
-    priceIndexes = []
-    for x, y in enumerate(AllPrices):
-        priceIndexes += [x]
+        priceIndexes = []
+        for x, y in enumerate(AllPrices):
+            priceIndexes += [x]
 
-    matplotlib.pyplot.scatter(priceIndexes, price, s=50, c='blue')
+        for COUNT, PRICES in enumerate(price):
+            if PRICES == max(price):
+                print('match')
+                maxPIndex = COUNT
+                matplotlib.pyplot.scatter(maxPIndex, PRICES, s=70, c='magenta')
+            else:
+                allPricesIndexes = COUNT
+                matplotlib.pyplot.scatter(allPricesIndexes, PRICES, s=50, c='blue')
 
-    for count, currentPrice in enumerate(AllPrices, 0):
-        try:
-            if count == len(AllPrices):
-                break
+        for count, currentPrice in enumerate(AllPrices, 0):
+            try:
+                if count == len(AllPrices):
+                    break
 
-            elif currentPrice > AllPrices[count + 1]:
-                matplotlib.pyplot.plot([priceIndexes[count], priceIndexes[count + 1]], [price[count], price[count + 1]],
-                                       c='red')
+                elif currentPrice > AllPrices[count + 1]:
+                    matplotlib.pyplot.plot([priceIndexes[count], priceIndexes[count + 1]],
+                                           [price[count], price[count + 1]],
+                                           c='red')
 
-            elif currentPrice <= AllPrices[count + 1]:
-                matplotlib.pyplot.plot([priceIndexes[count], priceIndexes[count + 1]], [price[count], price[count + 1]],
-                                       c='green')
-        except IndexError:
-            pass
+                elif currentPrice <= AllPrices[count + 1]:
+                    matplotlib.pyplot.plot([priceIndexes[count], priceIndexes[count + 1]],
+                                           [price[count], price[count + 1]],
+                                           c='green')
+            except IndexError:
+                pass
 
-    matplotlib.pyplot.xlabel('Index', fontsize=15)
-    matplotlib.pyplot.ylabel('Price', fontsize=15)
+        matplotlib.pyplot.xlabel('Index', fontsize=15)
+        matplotlib.pyplot.ylabel('Price', fontsize=15)
 
-    matplotlib.pyplot.title(stock, fontsize=20)
-    matplotlib.pyplot.show()
+        matplotlib.pyplot.title(stock, fontsize=20)
+        matplotlib.pyplot.show()
 
+    except OperationalError:
+        pass
