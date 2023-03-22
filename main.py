133a0748c08c10303f9be11a08a316f5bb42e59f
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-21 21:44:50 -0400

    Update main.py
    -Added new function that displays the difference of the current stock price and the highest scraped stock price
    -Added Time and Stocks labels 
    -Removed whitespace from URLs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from sqlite3 import OperationalError
 from bs4 import BeautifulSoup
 import requests
 import sqlite3
@@ -6,45 +5,9 @@ from datetime import datetime
 from tkinter import *
 import time
 from functions.autoRun import autoRun
+from functions.autoRun import stop_autorun
 from functions.plotPrices import showPlot
-
-# Initiates Tkinter
-root = Tk()
-root.geometry('365x124')
-
-# Original URL
-url = 'https://www.google.com/finance?q='
-
-# Connect to the database and enable the cursor
-conn = sqlite3.connect('data-stocks.db')
-c = conn.cursor()
-
-# Auto run count
-autoRunCount = 0
-
-# 'Stock' Label for the entry widget
-label_stock = Label(root, text='Stocks ')
-label_stock.grid(row=1, column=0, sticky='nsew')
-
-# Input the stocks into the Entry widget labeled 'Stocks'
-users_stocks = Entry(root)
-users_stocks.grid(row=1, column=1, columnspan=3, sticky='nsew')
-
-# 'Target price' Label for the entry widget
-target_price_label = Label(root, text='Target Price')
-target_price_label.grid(row=2, column=0, sticky='nsew')
-
-# Input for the price the user wants the stock to reach
-target_price = Entry(root)
-target_price.grid(row=2, column=1, columnspan=3, sticky='nsew')
-
-# Enter Button, when clicked the inputs will be collected, and separated, then web scraped from 'Google.com/finance' using BeautifulSoup4. The values will then be added to the database.
-button_enter = Button(root, text='Enter', command=lambda: search_it(users_stocks, target_price))
-button_enter.grid(row=1, column=4, sticky='nsew')
-
-# Auto run button
-button_autoRun = Button(root, text='Auto Run', command=lambda: autoRun(users_stocks, target_price, autoRunCount))
-button_autoRun.grid(row=2, column=4, sticky='nsew')
+from functions.upOrDown import fromHigh
 
 
 def search_it(STOCKS, TARGETPRICE):
@@ -81,15 +44,28 @@ def search_it(STOCKS, TARGETPRICE):
 
     # Function is called when 'Show' button is clicked
     def show_stocks():
+
+        # Time title label
+        label_time = Label(root, text='Time')
+        label_time.configure(foreground='red', font=('Arial', 20))
+        label_time.grid(row=3, column=0, sticky='nsew')
+
+        # Stocks title label
+        label_all_scraped_stocks = Label(root, text='Stocks')
+        label_all_scraped_stocks.configure(foreground='red', font=('Arial', 20))
+        label_all_scraped_stocks.grid(row=3, column=1, sticky='nsew')
+
+
         for numberOfStocks, stock in enumerate(scraped_stock_name):
             # Create the stock name label
-            label_stock_name = Label(root,
-                                     text=f'{scraped_stock_name[numberOfStocks]} is at ${scraped_stock_price[numberOfStocks]}')
-            label_stock_name.grid(row=3 + numberOfStocks, column=1, sticky='nsew')
+            label_stock_name = Label(root, text=f'{scraped_stock_name[numberOfStocks]} is at ${scraped_stock_price[numberOfStocks]}')
+            label_stock_name.grid(row=4 + numberOfStocks, column=1, sticky='nsew')
 
             # Timestamp of when the stock was scraped which will be displayed to the user when the show button is clicked.
             label_current_time = Label(root, text=timestamp)
-            label_current_time.grid(row=3 + numberOfStocks, column=0, sticky='nsew')
+            label_current_time.grid(row=4 + numberOfStocks, column=0, sticky='nsew')
+
+        fromHigh(stocks_split, root)
 
         root.update_idletasks()
         root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight()}")
@@ -97,7 +73,7 @@ def search_it(STOCKS, TARGETPRICE):
     # STOCK INFO
     for count, x in enumerate(stocks_split, start=0):
         # New URL with the stock added to it
-        url = f'https://www.google.com/finance?q={x}'
+        url = f'https://www.google.com/finance?q={x}'.replace(' ','')
 
         # Requests and scrape the URL
         req = requests.get(url)
@@ -179,6 +155,7 @@ def search_it(STOCKS, TARGETPRICE):
         for num, x in enumerate(stocks_split):
             showPlot(stocks_split[num])
 
+
         try:
             # If the price of the stock is less than the specified target price, a watchlist table will be created in the database and values will be added
             if price_float <= float(target_price[count]):
@@ -213,13 +190,60 @@ def search_it(STOCKS, TARGETPRICE):
 
 
 
+
+
         # IF an operational error occurs, 'Could not insert values into the watchlist' will be displayed
         except OperationalError:
             print('Operational Error: Could not insert values into the watchlist.')
 
+
+
         # URL resets to the original url for the next loop
         url = 'https://www.google.com/finance?q='
 
+
+
+
+# Initiates Tkinter
+root = Tk()
+root.geometry('365x124')
+
+# Original URL
+url = 'https://www.google.com/finance?q='
+
+# Connect to the database and enable the cursor
+conn = sqlite3.connect('data-stocks.db')
+c = conn.cursor()
+
+# Auto run count
+autoRunCount = 0
+
+# 'Stock' Label for the entry widget
+label_stock = Label(root, text='Stocks ')
+label_stock.grid(row=1, column=0, sticky='nsew')
+
+# Input the stocks into the Entry widget labeled 'Stocks'
+users_stocks = Entry(root)
+users_stocks.grid(row=1, column=1, columnspan=3, sticky='nsew')
+
+# 'Target price' Label for the entry widget
+target_price_label = Label(root, text='Target Price')
+target_price_label.grid(row=2, column=0, sticky='nsew')
+
+# Input for the price the user wants the stock to reach
+target_price = Entry(root)
+target_price.grid(row=2, column=1, columnspan=3, sticky='nsew')
+
+# Enter Button, when clicked the inputs will be collected, and separated, then web scraped from 'Google.com/finance' using BeautifulSoup4. The values will then be added to the database.
+button_enter = Button(root, text='Enter', command=lambda: search_it(users_stocks, target_price))
+button_enter.grid(row=1, column=4, sticky='nsew')
+
+# Auto run button
+button_autoRun = Button(root, text='Auto Run', command=lambda: autoRun(users_stocks, target_price, autoRunCount,root))
+button_autoRun.grid(row=2, column=4, sticky='nsew')
+
+button_autoRun_stop = Button(root, text='Stop', command=lambda: stop_autorun())
+button_autoRun_stop.grid(row=2, column=5)
 
 root.update_idletasks()
 root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight()}")
