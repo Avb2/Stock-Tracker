6b570580ec3cf856b1204478e82890d846fd8424
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-25 11:44:26 -0400

    Update previousClose.py
    - Removed border around the previous close value

--- functions/previousClose.py
+++ functions/previousClose.py
@@ -4,10 +4,11 @@ from tkinter import *
 
 
 def previousClose(stocks, root):
+
     label_previous_close_title = Label(root, text='Previous Close', foreground='red', font=('Arial', 20))
     label_previous_close_title.grid(row=3, column=7, sticky='nsew')
 
-    stocks = [stocks.get()]
+    stocks = stocks.split(',')
 
     for count, x in enumerate(stocks, start=0):
         # New URL with the stock added to it
@@ -29,9 +30,10 @@ def previousClose(stocks, root):
             print('I couldnt find that!')
             continue
 
-        label_previous_close = Label(root, text=f'{name}: {previousDayClose}', font=('Arial', 16), borderwidth=2,
-                                     highlightcolor='red', relief='solid')
+        label_previous_close = Label(root, text=f'{name}: {previousDayClose}', font=('Arial', 16))
         label_previous_close.grid(row=count + 4, column=7, sticky='nsew')
+
+        root.rowconfigure(count+4, pad=20)
 
     root.update_idletasks()
     root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight()}")
