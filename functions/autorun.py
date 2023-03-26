5591e6750fa597458b081b7343a01436d7e16a6e
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-25 22:38:19 -0400

    Update autorun.py
    -Added stop_autorun() function which ends the autoRun thread
    -Added smtplib to send emails when running autoRun if the stock is less than or equal to the target price

--- functions/autorun.py
+++ functions/autorun.py
@@ -1,33 +1,57 @@
 import time
-from sqlite3 import OperationalError
+import smtplib
 from bs4 import BeautifulSoup
 import requests
-import sqlite3
 from datetime import datetime
-from functions import plotPrices
 
 
-def autoRun(STOCKS, TARGETPRICE,AUTORUNCOUNT):
-    conn = sqlite3.connect('../data-stocks.db')
-    c = conn.cursor()
+def stop_autorun():
+    global autorunning
+    autorunning = False
+    print('Auto run stopped')
 
-    AUTORUNCOUNT += 2
-    while AUTORUNCOUNT % 2 == 0:
+
+def autoRun(STOCKS, TARGETPRICE):
+    def sendEmail(stock, emailcount):
+        smtp_server = 'smtp.office365.com'
+        smtp_port = 587
+        smtp_conn = smtplib.SMTP(smtp_server, smtp_port)
+
+        smtp_conn.starttls()
+        smtp_conn.login('youremail@outlook.com', 'yourpassword12')
+
+        from_addr = 'hotsauceinmypants@outlook.com'
+        to_addr = 'bringuel.alexander@gmail.com'
+
+        email_message = f'Subject: {stock} has hit the target \n\n {stock} has reached your target price of ${target_price[0]} and is currently at {price}. \n\n Date: {date}, Time: {timestamp}, Name: {name}, Price: {price}'
+        smtp_conn.sendmail(from_addr, to_addr, email_message)
+
+        print(f'Email sent to {to_addr}')
+        smtp_conn.quit()
+        emailcount += [1]
+
+
+    global autorunning
+    autorunning = True
+    print('Auto Run initiated')
+
+    # User inputs stocks they want to search for, the input is then split
+    users_stocks = str(STOCKS.get())
+    stocks_split = users_stocks.split(',')
+
+    # User inputs the target price they hope the stock reaches, the input is then split
+    target_price = TARGETPRICE.get()
+    target_price = target_price.split(',')
+
+    emailcount = []
+
+    while autorunning:
         # Scraped stock names are added to this list
         scraped_stock_name = []
 
         # Scraped prices are added to this list
         scraped_stock_price = []
 
-        # User inputs stocks they want to search for, the input is then split
-        users_stocks = str(STOCKS.get())
-        stocks_split = users_stocks.split(',')
-        print('Stocks split', stocks_split)
-
-        # User inputs the target price they hope the stock reaches, the input is then split
-        target_price = TARGETPRICE.get()
-        target_price = target_price.split(',')
-        print('target price', target_price)
         # STOCK INFO
         for count, x in enumerate(stocks_split, start=0):
             # New URL with the stock added to it
@@ -47,7 +71,6 @@ def autoRun(STOCKS, TARGETPRICE,AUTORUNCOUNT):
 
             # Finds the price of the stock and converts it into a float
             price = (result.find('div', {'class': 'YMlKec fxKbKc'})).string
-            print(price)
             price_float = float(price[1:])
             scraped_stock_price += [price_float]
 
@@ -57,101 +80,14 @@ def autoRun(STOCKS, TARGETPRICE,AUTORUNCOUNT):
             # Date of when the data is being logged
             date = str(datetime.now())[:11]
 
-            try:
-                c.execute('''CREATE TABLE AllStocks
-                            (
-                            Name,
-                            Date,
-                            Time,
-                            Price
-                            )''')
-                conn.commit()
+            print(f'Date: {date}, Time: {timestamp}, Name: {name}, Price: {price}')
 
-
-            except OperationalError:
-                print('Operational Error: Could not create table.')
-
-            # Adds the values to the database
-            c.execute(f'''INSERT INTO AllStocks VALUES
-                            (
-                            "{name}",
-                            "{date}",
-                            "{timestamp}",
-                            "${price_float}"
-                            )''')
-            conn.commit()
-
-            # Use a try/ except statement to test if a table with the same name already exists. If the table exists, the values will be added to the already existing table.
-            try:
-                # Create table for the stock
-                c.execute(f'''CREATE TABLE {x}
-                                (
-                                Name,
-                                Date,    
-                                Time,
-                                Price
-                                )''')
-
-                conn.commit()
-
-            except OperationalError:
-                print('Operational Error: Could not create table.')
-
-            # Adds the values to the database
-            c.execute(f'''INSERT INTO {x} VALUES
-                            (
-                            "{name}",
-                            "{date}",
-                            "{timestamp}",
-                            "${price_float}"
-                            )''')
-            conn.commit()
-
-            # 'Finished' will be printed when the values have been added.
-            print('finished')
-
-            plotPrices.showPlot(stocks_split)
-
-            try:
-                # If the price of the stock is less than the specified target price, a watchlist table will be created in the database and values will be added
-                if price_float <= float(target_price[count]):
-
-                    # Creates a table called watchlist if one isnt created already. If an Operational Error occurs, 'Could not create table' will be displayed.
-                    try:
-                        c.execute('''CREATE TABLE Watchlist
-                                    (
-                                    Name,
-                                    Date,
-                                    Time,
-                                    Price
-                                    )''')
-
-
-                    except OperationalError:
-                        print('Operational Error: Could not create table.')
-
-                    # Adds values to the Watchlist database if the target price is greater than the current price
-                    c.execute(f'''INSERT INTO Watchlist VALUES
-                                (
-                                "{name}",
-                                "{date}",                    
-                                "{timestamp}",
-                                "${price_float}"
-                                )''')
-
-                    conn.commit()
-
-                    # Tells the user that the stock was added to the watchlist
-                    print('Stock added to watchlist')
-
-
-
-            # IF an operational error occurs, 'Could not insert values into the watchlist' will be displayed
-            except OperationalError:
-                print('Operational Error: Could not insert values into the watchlist.')
-
-            time.sleep()
+            if price_float <= int(target_price[0]) and len(emailcount) < 1:
+                sendEmail(name, emailcount)
 
             # URL resets to the original url for the next loop
             url = 'https://www.google.com/finance?q='
 
+            # Sleeps for 10 seconds
+            print(autorunning)
+            time.sleep(600)
