bb69d8af927c58d6e26415170e04a83df534567a
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-28 14:27:19 -0400

    Add files via upload

--- IN PROGRESS/newFunctions/scrapeStock.py
+++ IN PROGRESS/newFunctions/scrapeStock.py
@@ -56,35 +56,35 @@ def collect_stock_info(root, stockInputField, targetPriceInputField, lock):
 
             # Time/ Date Title label
             timeDateTitleLabel = Label(root, text='Date/Time', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            timeDateTitleLabel.grid(row=3, column=1, sticky='nsew')
+            timeDateTitleLabel.grid(row=4, column=1, sticky='nsew')
 
             # Stock Title label
             stockTitleLabel = Label(root, text='Name', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            stockTitleLabel.grid(row=3, column=2, sticky='nsew')
+            stockTitleLabel.grid(row=4, column=2, sticky='nsew')
 
             # Price Title label
             priceTitleLabel = Label(root, text='Price', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            priceTitleLabel.grid(row=3, column=3, sticky='nsew')
+            priceTitleLabel.grid(row=4, column=3, sticky='nsew')
 
             # Closing price Title label
             closingPriceTitleLabel = Label(root, text='Closing Price', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            closingPriceTitleLabel.grid(row=3, column=4, sticky='nsew')
+            closingPriceTitleLabel.grid(row=4, column=4, sticky='nsew')
 
             # Displays current time/date
             currentTimeDateLabel = Label(root, text=f'{date} : {time}', font=('Arial', 16))
-            currentTimeDateLabel.grid(row=4 + index, column=1, sticky='nsew')
+            currentTimeDateLabel.grid(row=5 + index, column=1, sticky='nsew')
 
             # Displays stock name
             stockNameLabel = Label(root, text=f'{stockName}: ', font=('Arial', 16))
-            stockNameLabel.grid(row=4 + index, column=2, sticky='nsew')
+            stockNameLabel.grid(row=5 + index, column=2, sticky='nsew')
 
             # Displays stock price
             stockPriceLabel = Label(root, text=stockPrice, font=('Arial', 16))
-            stockPriceLabel.grid(row=4 + index, column=3, sticky='nsew')
+            stockPriceLabel.grid(row=5 + index, column=3, sticky='nsew')
 
             # Displays Previous closing price
             stockPreviousClosingPriceLabel = Label(root, text=stockPreviousClosingPrice, font=('Arial', 16))
-            stockPreviousClosingPriceLabel.grid(row=4 + index, column=4, sticky='nsew')
+            stockPreviousClosingPriceLabel.grid(row=5 + index, column=4, sticky='nsew')
 
             # Adds padding between all rows of stock information
             for num in range(index):
