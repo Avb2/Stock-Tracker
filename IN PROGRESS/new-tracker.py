207bece5c4430c4bb463cfce36d393609791da34
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-03-28 02:33:08 -0400

    Add files via upload
    - Formatting

--- IN PROGRESS/new-tracker.py
+++ IN PROGRESS/new-tracker.py
@@ -1,15 +1,23 @@
+import threading
 from tkinter import *
 from tkinter.ttk import Combobox
 from newFunctions.scrapeStock import collect_stock_info
 
+
+lock = threading.Lock()
+
 # Initialize tkinter widget
 root = Tk()
-root.columnconfigure(1, pad=20)
+root.columnconfigure(1, pad=8)
+root.columnconfigure(2, pad=8)
+root.columnconfigure(3, pad=8)
+root.columnconfigure(4, pad=8)
+root.rowconfigure(2, pad=15)
 
 # Stock title label
 stockTitleLabel = Label(root, text='Stocks', foreground='red')
-stockTitleLabel.configure(font=('Arial', 16))
-stockTitleLabel.grid(row=1, column=1)
+stockTitleLabel.configure(font=('Arial', 18))
+stockTitleLabel.grid(row=1, column=1, sticky='nsew')
 
 # Stock input field
 stockInputField = Combobox(root)
@@ -17,16 +25,20 @@ stockInputField.grid(row=2, column=1)
 
 # Target price title label
 targetPriceTitleLabel = Label(root, text='Target Price', foreground='red')
-targetPriceTitleLabel.configure(font=('Arial', 16))
-targetPriceTitleLabel.grid(row=1, column=2)
+targetPriceTitleLabel.configure(font=('Arial', 18))
+targetPriceTitleLabel.grid(row=1, column=2, sticky='nsew')
 
 # Target price input field
 targetPriceInputField = Entry(root)
 targetPriceInputField.grid(row=2, column=2)
 
 # Enter Button
-buttonEnter = Button(root, text='Enter', activeforeground='magenta', command=lambda: collect_stock_info(root, stockInputField, targetPriceInputField))
-buttonEnter.grid(row=2, column=3)
+buttonEnter = Button(root, text='Enter', activeforeground='magenta', font=('Arial', 16), command=lambda: collect_stock_info(root, stockInputField, targetPriceInputField, lock))
+buttonEnter.grid(row=2, column=3, sticky='nsew')
 
 # Run tkinter root
 root.mainloop()
+
+root.update_idletasks()
+root.geometry(f"{root.winfo_reqwidth()}x{root.winfo_reqheight()}")
+
