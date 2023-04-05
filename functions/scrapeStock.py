00e156babaafb51a259ebb4105cb74ad702e6507
Avb2 <114643524+Avb2@users.noreply.github.com>
2023-04-05 12:04:08 -0400

    Add files via upload
    -Organization changes

--- functions/scrapeStock.py
+++ functions/scrapeStock.py
@@ -8,6 +8,59 @@ from functions.databaseQuerying import add_to_db_for_sectors
 from functions.modelStocks import showPlot
 
 
+def collect_time():
+    # Get timestamp
+    time = str(datetime.now())[11:16]
+    return time
+
+
+def collect_date():
+    date = str(datetime.now())[:11]
+    return date
+
+
+def create_stock_info_labels(root, stockName, stockPrice, stockPreviousClosingPrice, index):
+    time = collect_time()
+    date = collect_date()
+
+    # Time/ Date Title label
+    timeDateTitleLabel = Label(root, text='Date/Time', borderwidth=1.5, relief='solid', font=('Arial', 16))
+    timeDateTitleLabel.grid(row=4, column=1, sticky='nsew')
+
+    # Stock Title label
+    stockTitleLabel = Label(root, text='Name', borderwidth=1.5, relief='solid', font=('Arial', 16))
+    stockTitleLabel.grid(row=4, column=2, sticky='nsew')
+
+    # Price Title label
+    priceTitleLabel = Label(root, text='Price', borderwidth=1.5, relief='solid', font=('Arial', 16))
+    priceTitleLabel.grid(row=4, column=3, sticky='nsew')
+
+    # Closing price Title label
+    closingPriceTitleLabel = Label(root, text='Closing Price', borderwidth=1.5, relief='solid',
+                                   font=('Arial', 16))
+    closingPriceTitleLabel.grid(row=4, column=4, sticky='nsew')
+
+    # Displays current time/date
+    currentTimeDateLabel = Label(root, text=f'{date} : {time}', font=('Arial', 16))
+    currentTimeDateLabel.grid(row=5 + index, column=1, sticky='nsew')
+
+    # Displays stock name
+    stockNameLabel = Label(root, text=f'{stockName}: ', font=('Arial', 16))
+    stockNameLabel.grid(row=5 + index, column=2, sticky='nsew')
+
+    # Displays stock price
+    stockPriceLabel = Label(root, text=stockPrice, font=('Arial', 16))
+    stockPriceLabel.grid(row=5 + index, column=3, sticky='nsew')
+
+    # Displays Previous closing price
+    stockPreviousClosingPriceLabel = Label(root, text=stockPreviousClosingPrice, font=('Arial', 16))
+    stockPreviousClosingPriceLabel.grid(row=5 + index, column=4, sticky='nsew')
+
+    # Adds padding between all rows of stock information
+    for num in range(index):
+        root.rowconfigure(num + 4, pad=10)
+
+
 def collect_stock_info(root, stockInputField, targetPriceInputField, lock):
     def scrape_stock_info_for_sectors(root, stockInputField):
         def scrape_sector_list(sector):
@@ -38,63 +91,13 @@ def collect_stock_info(root, stockInputField, targetPriceInputField, lock):
                 # Find the previous closing price
                 stockPreviousClosingPrice = result.find('div', {'class': 'P6K39c'}).string
 
-                def collect_time():
-                    # Get timestamp
-                    time = str(datetime.now())[11:16]
-                    return time
-
-                def collect_date():
-                    date = str(datetime.now())[:11]
-                    return date
-
-                def create_stock_info_labels(root):
-                    time = collect_time()
-                    date = collect_date()
-
-                    # Time/ Date Title label
-                    timeDateTitleLabel = Label(root, text='Date/Time', borderwidth=1.5, relief='solid',
-                                               font=('Arial', 16))
-                    timeDateTitleLabel.grid(row=4, column=1, sticky='nsew')
-
-                    # Stock Title label
-                    stockTitleLabel = Label(root, text='Name', borderwidth=1.5, relief='solid', font=('Arial', 16))
-                    stockTitleLabel.grid(row=4, column=2, sticky='nsew')
-
-                    # Price Title label
-                    priceTitleLabel = Label(root, text='Price', borderwidth=1.5, relief='solid', font=('Arial', 16))
-                    priceTitleLabel.grid(row=4, column=3, sticky='nsew')
-
-                    # Closing price Title label
-                    closingPriceTitleLabel = Label(root, text='Closing Price', borderwidth=1.5, relief='solid',
-                                                   font=('Arial', 16))
-                    closingPriceTitleLabel.grid(row=4, column=4, sticky='nsew')
-
-                    # Displays current time/date
-                    currentTimeDateLabel = Label(root, text=f'{date} : {time}', font=('Arial', 16))
-                    currentTimeDateLabel.grid(row=5 + index, column=1, sticky='nsew')
-
-                    # Displays stock name
-                    stockNameLabel = Label(root, text=f'{stockName}: ', font=('Arial', 16))
-                    stockNameLabel.grid(row=5 + index, column=2, sticky='nsew')
-
-                    # Displays stock price
-                    stockPriceLabel = Label(root, text=stockPrice, font=('Arial', 16))
-                    stockPriceLabel.grid(row=5 + index, column=3, sticky='nsew')
-
-                    # Displays Previous closing price
-                    stockPreviousClosingPriceLabel = Label(root, text=stockPreviousClosingPrice, font=('Arial', 16))
-                    stockPreviousClosingPriceLabel.grid(row=5 + index, column=4, sticky='nsew')
-
-                    # Adds padding between all rows of stock information
-                    for num in range(index):
-                        root.rowconfigure(num + 4, pad=10)
-
-                create_stock_info_labels(root)
+                create_stock_info_labels(root, stockName, stockPrice, stockPreviousClosingPrice, index)
 
                 theTime = collect_time()
                 date = collect_date()
 
-                add_to_db_for_sectors(stockInputField.get(), stockName, date, theTime, round(float(stockPrice.replace('$', '')), 2))
+                add_to_db_for_sectors(stockInputField.get(), stockName, date, theTime,
+                                      round(float(stockPrice.replace('$', '')), 2))
 
         techSector = [
             'AAPL',
@@ -174,6 +177,9 @@ def collect_stock_info(root, stockInputField, targetPriceInputField, lock):
             scrape_sector_thread.start()
 
     def scrape_stock_info(stockBeingScraped, index):
+        def create_graph_button(root):
+            buttonGraphPrices = Button(root, text='Graph', command=lambda: showPlot(stockName))
+            buttonGraphPrices.grid(row=5 + index, column=5)
 
         # Adds the stock being scraped from the stock list to the url
         url = f'https://www.google.com/finance?q={stockBeingScraped}'.replace(' ', '')
@@ -204,67 +210,13 @@ def collect_stock_info(root, stockInputField, targetPriceInputField, lock):
         # Find the previous closing price
         stockPreviousClosingPrice = result.find('div', {'class': 'P6K39c'}).string
 
-        def collect_time():
-            # Get timestamp
-            time = str(datetime.now())[11:16]
-            return time
-
-        def collect_date():
-            date = str(datetime.now())[:11]
-            return date
-
-        def create_graph_button(root):
-            buttonGraphPrices = Button(root, text='Graph', command=lambda: showPlot(stockName))
-            buttonGraphPrices.grid(row=5 + index, column=5)
-
-        def create_stock_info_labels(root):
-            time = collect_time()
-            date = collect_date()
-
-            # Time/ Date Title label
-            timeDateTitleLabel = Label(root, text='Date/Time', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            timeDateTitleLabel.grid(row=4, column=1, sticky='nsew')
-
-            # Stock Title label
-            stockTitleLabel = Label(root, text='Name', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            stockTitleLabel.grid(row=4, column=2, sticky='nsew')
-
-            # Price Title label
-            priceTitleLabel = Label(root, text='Price', borderwidth=1.5, relief='solid', font=('Arial', 16))
-            priceTitleLabel.grid(row=4, column=3, sticky='nsew')
-
-            # Closing price Title label
-            closingPriceTitleLabel = Label(root, text='Closing Price', borderwidth=1.5, relief='solid',
-                                           font=('Arial', 16))
-            closingPriceTitleLabel.grid(row=4, column=4, sticky='nsew')
-
-            # Displays current time/date
-            currentTimeDateLabel = Label(root, text=f'{date} : {time}', font=('Arial', 16))
-            currentTimeDateLabel.grid(row=5 + index, column=1, sticky='nsew')
-
-            # Displays stock name
-            stockNameLabel = Label(root, text=f'{stockName}: ', font=('Arial', 16))
-            stockNameLabel.grid(row=5 + index, column=2, sticky='nsew')
-
-            # Displays stock price
-            stockPriceLabel = Label(root, text=stockPrice, font=('Arial', 16))
-            stockPriceLabel.grid(row=5 + index, column=3, sticky='nsew')
-
-            # Displays Previous closing price
-            stockPreviousClosingPriceLabel = Label(root, text=stockPreviousClosingPrice, font=('Arial', 16))
-            stockPreviousClosingPriceLabel.grid(row=5 + index, column=4, sticky='nsew')
-
-            # Adds padding between all rows of stock information
-            for num in range(index):
-                root.rowconfigure(num + 4, pad=10)
-
-        create_stock_info_labels(root)
+        create_stock_info_labels(root, stockName, stockPrice, stockPreviousClosingPrice, index)
 
         theTime = collect_time()
         date = collect_date()
 
         add_to_db(SearchedBy=SearchedBy[index], name=stockName, priceFloat=round(float(stockPrice.replace('$', '')), 2),
-            date=date, time=theTime, targetPrice=listOfTargetPrices[index])
+                  date=date, time=theTime, targetPrice=listOfTargetPrices[index])
         create_graph_button(root)
 
     lock.acquire()
